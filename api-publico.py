2722298d7d19980c60b054719bfd5478a3a17833
Benjamin Valiente <79182724+benjaminvaliente@users.noreply.github.com>
2023-10-20 09:26:01 -0600

    Subiendo ejemplo api

---
+++ api-publico.py
@@ -0,0 +1,8 @@
+import json
+
+def lambda_handler(event, context):
+    response = {
+        "statusCode": 200,
+        "body": json.dumps("¡Hola desde AWS Lambda! Mi nombre es..."),
+    }
+    return response

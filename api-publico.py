1a5d300d37732f4e12e234b60228fed4d2171b8c
Wilhelm Otzoy <34988061+wros21@users.noreply.github.com>
2023-11-14 12:33:53 -0600

    Update api-publico.py
    add name

--- api-publico.py
+++ api-publico.py
@@ -3,6 +3,6 @@ import json
 def lambda_handler(event, context):
     response = {
         "statusCode": 200,
-        "body": json.dumps("¡Hola desde AWS Lambda! Mi nombre es..."),
+        "body": json.dumps("¡Hola desde AWS Lambda! Mi nombre es Wilhelm Otzoy"),
     }
     return response
